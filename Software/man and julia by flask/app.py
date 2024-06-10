df0b9150f345729ad0b9c7ad06878290a13ce650
LYCMIA03 <lycmia03@gmail.com>
2024-06-10 13:23:09 +0100

    Updated

--- Software/man and julia by flask/app.py
+++ Software/man and julia by flask/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, request, jsonify, send_file
+import base64
+
+from flask import Flask, request, send_file, jsonify
 from io import BytesIO
 import numpy as np
 from PIL import Image
-import julia_cy  # 导入Cython模块
+import julia_cy  # 假设这个模块已经正确编译并可以导入
 from flask_cors import CORS
 import time
 
@@ -10,7 +12,7 @@ app = Flask(__name__, static_folder='static')
 CORS(app)
 
 # Image dimensions
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 640, 480
 
 # Mandelbrot set range
 REAL_MIN, REAL_MAX = -2.0, 1.0
@@ -19,57 +21,52 @@ IMAG_MIN, IMAG_MAX = -1.5, 1.5
 # Maximum iterations
 MAX_ITER = 100
 
+
 def generate_image(data_set, iteration, max_iter):
     image = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
     julia_cy.generate_image(data_set, iteration, WIDTH, HEIGHT, image)
     return Image.fromarray(image)
 
+
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
+
 
 @app.route('/mandelbrot', methods=['GET'])
 def compute_and_display_mandelbrot():
     mandelbrot_set = np.zeros((WIDTH, HEIGHT), dtype=int)
 
     # Start timing
-    start_time = time.time()
+    start_time = time.perf_counter_ns()
 
     # Compute Mandelbrot set
-    frame_times = []
-    for iteration in range(1, MAX_ITER + 1):
-        frame_start_time = time.time()
-        julia_cy.compute_mandelbrot_set(mandelbrot_set, REAL_MIN, REAL_MAX, IMAG_MIN, IMAG_MAX, MAX_ITER, iteration)
-        frame_end_time = time.time()
-        frame_times.append(frame_end_time - frame_start_time)
+    julia_cy.compute_mandelbrot_set(mandelbrot_set, REAL_MIN, REAL_MAX, IMAG_MIN, IMAG_MAX, MAX_ITER, MAX_ITER)
 
     # Generate the final image
     mandelbrot_image = generate_image(mandelbrot_set, MAX_ITER, MAX_ITER)
 
     # End timing
-    end_time = time.time()
+    end_time = time.perf_counter_ns()
 
     # Calculate total time, throughput, and latency
     total_time = end_time - start_time
     pixel_count = WIDTH * HEIGHT * MAX_ITER
-    overall_throughput = pixel_count / total_time
-    latency = total_time / pixel_count
+    overall_throughput = (pixel_count / total_time) * 1e9
+    latency = (total_time / pixel_count)*1e-9
 
-    # Calculate frame rate
-    total_frame_time = sum(frame_times)
-    fps = len(frame_times) / total_frame_time
-
-    print(f"Total time: {total_time:.4f} seconds")
+    print(f"Total time: {total_time/1e9:.4f} seconds")
     print(f"Overall throughput: {overall_throughput:.2f} pixels per second")
     print(f"Latency per pixel: {latency:.6f} seconds")
-    print(f"Average FPS: {fps:.2f} frames per second")
 
     # Save image to a BytesIO object
     img_io = BytesIO()
     mandelbrot_image.save(img_io, 'PNG')
     img_io.seek(0)
+    total_time = total_time / 1e9
+    return jsonify({"image": 'data:image/png;base64,' + base64.b64encode(img_io.getvalue()).decode('utf-8'),
+                    'total_time': total_time, 'overall_throughput': overall_throughput, 'latency': latency})
 
-    return send_file(img_io, mimetype='image/png')
 
 @app.route('/julia', methods=['POST'])
 def compute_and_display_julia():
@@ -81,37 +78,37 @@ def compute_and_display_julia():
     julia_set = np.zeros((WIDTH, HEIGHT), dtype=int)
 
     # Start timing
-    start_time = time.time()
+    start_time = time.perf_counter_ns()
 
     # Compute Julia set
     julia_cy.compute_julia_set(julia_set, REAL_MIN, REAL_MAX, IMAG_MIN, IMAG_MAX, cr, ci, MAX_ITER)
 
     # End timing
-    end_time = time.time()
+    end_time = time.perf_counter_ns()
 
     # Generate the final image
     julia_image = generate_image(julia_set, MAX_ITER, MAX_ITER)
 
-    # Calculate total time, throughput, and latency
-    total_time = end_time - start_time
+    # Calculate total time, throughput, and latency in seconds
+    total_time = (end_time - start_time)
+
     pixel_count = WIDTH * HEIGHT * MAX_ITER
-    overall_throughput = pixel_count / total_time
-    latency = total_time / pixel_count
+    overall_throughput = (pixel_count / total_time) * 1e9
+    latency = (total_time / pixel_count) * 1e-9
 
-    # Calculate frame rate
-    fps = 1 / (total_time / MAX_ITER)
-
-    print(f"Total time: {total_time:.4f} seconds")
+    print(f"Total time: {total_time/1e9:.4f} seconds")
     print(f"Overall throughput: {overall_throughput:.2f} pixels per second")
     print(f"Latency per pixel: {latency:.6f} seconds")
-    print(f"Average FPS: {fps:.2f} frames per second")
 
     # Save image to a BytesIO object
     img_io = BytesIO()
     julia_image.save(img_io, 'PNG')
     img_io.seek(0)
+    total_time = total_time / 1e9
+    return jsonify(
+        {'image': 'data:image/png;base64,' + base64.b64encode(img_io.getvalue()).decode('utf-8'), 'total_time': total_time,
+         'overall_throughput': overall_throughput, 'latency': latency})
 
-    return send_file(img_io, mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
